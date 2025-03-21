84cf055dc0c76bc1a3b5e854d78e0e52e46aecc3
Talha <talhauyanik3@gmail.com>
2025-03-21 17:44:32 +0300

    Add Firebase configuration file and update .gitignore

---
+++ config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAkmtB79Rt1gzfRb7-R5hBaXhNln6hcAlA",
+    authDomain: "sensordata-e720d.firebaseapp.com",
+    databaseURL: "https://sensordata-e720d-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "sensordata-e720d",
+    storageBucket: "sensordata-e720d.firebasestorage.app",
+    messagingSenderId: "134640996581",
+    appId: "1:134640996581:web:33da07f917360256a2040c",
+    measurementId: "G-B32EC2D1E6"
+  };
+
+export default firebaseConfig;
